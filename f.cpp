ac31064f8bf1c63389eab32b632f9e209369e611
Arkhip <108357450+vvmutee@users.noreply.github.com>
2025-05-30 09:22:09 +0200

    Update f.cpp

--- f.cpp
+++ f.cpp
@@ -1,135 +1,176 @@
 #include <iostream>
+#include <iomanip>
 #include <stdexcept>
 
-template<typename T, size_t N, size_t M>
+
+template <typename T>
 class Matrix {
-    static_assert(N <= 3 && M <= 3, "Matrix dimensions must be <= 3");
+    int n, m;  // n - кол-во строк; m - кол-во столбцов
+    T** data; //массив массивов, поэтому 2 звезды
 
-private:
-    T data[N][M]{};
 
-public:
-    // Базовый конструктор
-    Matrix() = default;
-
-    // Конструктор копирования
-    Matrix(const Matrix& other) {
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < M; ++j)
-                data[i][j] = other.data[i][j];
+   public:
+    // Основной (базовый) конструктор
+    Matrix(int rows, int cols) {
+        std::cout << "Constr" << std::endl;
+        n = rows;
+        m = cols;
+        data = new T* [n];//будут содержаться адреса массивов
+        for (int i = 0; i < n; i++) data[i] = new T [m];//массив массивов, поэтому 2 звезды
     }
 
-    // Оператор присваивания
+
+    // Конструктор копирования для создания новой матрицы на основе предыдущей
+    Matrix(const Matrix& other) : Matrix(other.n, other.m) {
+        std::cout << "Copy Constr" << std::endl;
+
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) data[i][j] = other.data[i][j];
+    }
+
+
+    // Оператор присваивания копированием  для копирования существующей в существующую
     Matrix& operator=(const Matrix& other) {
-        if (this != &other) {
-            for (size_t i = 0; i < N; ++i)
-                for (size_t j = 0; j < M; ++j)
-                    data[i][j] = other.data[i][j];
-        }
+        for (int i = 0; i < n; i++) delete[] data[i]; //удаляем предыдущие данные, чтобы присвоить новое
+
+
+        delete[] data;
+
+
+        n = other.n;
+        m = other.m;
+
+
+        data = new T*[n];
+        for (int i = 0; i < n; i++) data[i] = new T[m];
+
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) data[i][j] = other.data[i][j];
+
+
         return *this;
     }
 
-    // Оператор ввода
-    friend std::istream& operator>>(std::istream& in, Matrix& m) {
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < M; ++j)
-                in >> m.data[i][j];
+
+    // Деструктор Он вызывается автоматически, мы его не вызываем
+    ~Matrix() {
+        std::cout << "Destr before" << std::endl;
+        for (int i = 0; i < n; i++) delete[] data[i];
+
+
+        delete[] data;
+        std::cout << "Destr after" << std::endl;
+    }
+
+
+    friend Matrix operator + (const Matrix& a, const Matrix& b) //дружественная функция не является приватной, но имеет доступ к приватным данным класса
+    {
+        if (a.n != b.n || a.m != b.m)
+            throw std::length_error("Sizes do not match.");
+
+
+        Matrix c = Matrix(a.n, a.m);
+
+
+        for (int i = 0; i < c.n; i++)
+            for (int j = 0; j < c.m; j++)
+                c.data[i][j] = a.data[i][j] + b.data[i][j];
+
+
+        return c;
+    }
+   
+    friend std::ostream& operator << (std::ostream& os, const Matrix& a)
+    {
+        for (int i = 0; i < a.n; i++) {
+            for (int j = 0; j < a.m; j++)
+                os << std::setw(5) << a.data[i][j];
+            os << std::endl;
+        }
+        return os;
+    }
+
+
+    T * operator [] (int i)
+    {
+        return data[i];
+    }
+    friend std::istream& operator >>(std::istream& in, const Matrix& a) //оператор ввода
+    {
+        for(int i = 0; i < a.n; i++)
+            for(int j = 0; j < a.m; j++)
+                in >> a.data[i][j];
         return in;
     }
 
-    // Оператор вывода
-    friend std::ostream& operator<<(std::ostream& out, const Matrix& m) {
-        for (size_t i = 0; i < N; ++i) {
-            for (size_t j = 0; j < M; ++j)
-                out << m.data[i][j] << ' ';
-            out << '\n';
-        }
-        return out;
-    }
 
-    // Оператор +
-    Matrix operator+(const Matrix& other) const {
-        Matrix result;
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < M; ++j)
-                result.data[i][j] = data[i][j] + other.data[i][j];
+    Matrix&  operator += (const Matrix& other)
+    {
+        if(n != other.n || m != other.m)
+            throw std::length_error("Sizes do not match.");
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+               this->data[i][j] += other.data[i][j];
+       return *this;
+    }
+    friend Matrix operator * (const Matrix& a, const Matrix& b)
+    {
+        if(a.m != b.n)
+            throw std::length_error("Sizes do not match.");
+        Matrix result(a.n, b.m);
+
+
+        for (int i = 0; i < a.n; i++)
+            for (int j = 0; j < b.m; j++)
+                for(int k = 0; k < a.m; k++)
+                    result[i][j] += a.data[i][k] * b.data[k][j];
         return result;
     }
-
-    // Оператор +=
-    Matrix& operator+=(const Matrix& other) {
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < M; ++j)
-                data[i][j] += other.data[i][j];
+    Matrix&  operator *= (const Matrix& other)
+    {
+        if(m != other.n)
+            throw std::length_error("Sizes do not match.");
+       
+        *this = *this * other;    
         return *this;
     }
-
-    // Оператор *
-    Matrix operator*(const Matrix<T, M, N>& other) const {
-        static_assert(N == M, "Matrix multiplication requires N == M for square matrices");
-        Matrix<T, N, N> result;
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < N; ++j)
-                for (size_t k = 0; k < M; ++k)
-                    result.data[i][j] += data[i][k] * other.data[k][j];
-        return result;
-    }
-
-    // Оператор *=
-    Matrix& operator*=(const T& scalar) {
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < M; ++j)
-                data[i][j] *= scalar;
+    Matrix& operator ++( )
+    {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                this -> data[i][j]++;
         return *this;
     }
-
-    // Оператор ++ (префикс)
-    Matrix& operator++() {
-        for (size_t i = 0; i < N; ++i)
-            for (size_t j = 0; j < M; ++j)
-                ++data[i][j];
-        return *this;
-    }
-
-    // Доступ к элементу (по ссылке для изменения)
-    T& operator()(size_t i, size_t j) {
-        if (i >= N || j >= M) throw std::out_of_range("Index out of bounds");
-        return data[i][j];
-    }
-
-    // Доступ к элементу (только для чтения)
-    const T& operator()(size_t i, size_t j) const {
-        if (i >= N || j >= M) throw std::out_of_range("Index out of bounds");
-        return data[i][j];
-    }
-
-    // Метод вычисления определителя (только для квадратных 2x2 и 3x3)
-    T determinant() const {
-        static_assert(N == M, "Determinant can only be calculated for square matrices");
-
-        if constexpr (N == 1) {
+    T det()
+    {
+        if(n != m) throw std::length_error("Sizes do not match.");
+        if(n == 1)
             return data[0][0];
-        } else if constexpr (N == 2) {
+        if(n == 2)
             return data[0][0] * data[1][1] - data[0][1] * data[1][0];
-        } else if constexpr (N == 3) {
-            return
-                data[0][0] * (data[1][1] * data[2][2] - data[1][2] * data[2][1]) -
-                data[0][1] * (data[1][0] * data[2][2] - data[1][2] * data[2][0]) +
-                data[0][2] * (data[1][0] * data[2][1] - data[1][1] * data[2][0]);
-        } else {
-            throw std::logic_error("Determinant not implemented for this size");
-        }
+        if(n == 3)
+            return data[0][0] * data[1][1] * data[2][2] - data[0][0] * data[1][2] * data[2][1] - data[0][1] * data[1][0] * data[2][2] + data[0][1] * data[1][2] * data[2][0] + data[0][2] * data[1][0] * data[2][1] - data[0][2] * data[1][1] * data[2][1];
     }
-};
-int main() {
-    Matrix<int, 2, 2> A, B;
-    std::cin >> A >> B;
-    Matrix<int, 2, 2> C = A + B;
-    std::cout << "C = A + B:\n" << C;
-    ++C;
-    std::cout << "C после ++:\n" << C;
-    std::cout << "Определитель C: " << C.determinant() << "\n";
-    C(0, 1) = 99;
-    std::cout << "После изменения элемента C(0,1):\n" << C;
-}
 
+};
+
+
+
+
+
+int main()
+{
+    Matrix<int> a(3, 3);
+    std::cin >> a;
+    Matrix<int> c(3, 3);
+    std::cin >> c;
+
+
+    std::cout << ++a << std::endl;
+    std::cout << c << std::endl;
+    int y = c.det();
+    std::cout << y <<std::endl;
+    return 0;
+}
